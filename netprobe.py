bdb7e8463bc0deafd956aea6be697781cc1ddbb6
Emir Akdag <emirakdag@protonmail.com>
2023-11-11 17:27:20 +0300

    FIX nargs in port-scan and handle with arg counts.

--- netprobe.py
+++ netprobe.py
@@ -9,7 +9,7 @@ def main():
                        metavar='IPv4',
                        type=str,
                        action='store',
-                       nargs=(0,3),
+                       nargs='*',
                        help='Perform a port scan on the specified host. Usage: -p <host> [start-port] [end-port]')
     group.add_argument('-n', '--network-scan',
                        metavar='IPv4',
@@ -22,6 +22,8 @@ def main():
     if args.network_scan:
         pass
     elif args.port_scan:
+        if len(args.port_scan) > 3:
+            parser.error("Too many arguments for port-scan. Maximum allowed: 3.")
         host, *port_range = args.port_scan
         # Check port_range is not empty and check first elemnt of port_range is not empty. If empty set value of 1.
         start_port = int(port_range[0] if port_range and port_range[0] else 1)
